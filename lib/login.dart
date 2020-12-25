f117427f7fcea2ac14dbbb4c4b8148a3883187dd
Taber Bin Zameer <tbzameer@gmail.com>
2020-12-26 03:35:49 +0500

    register

--- lib/login.dart
+++ lib/login.dart
@@ -14,184 +14,188 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-            Text(
-              "Sign In",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-            Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.tealAccent),
-                          borderRadius: BorderRadius.circular(5)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      hintText: 'sarahkaramsi@gmail.com',
-                      hintStyle: TextStyle(
-                        fontSize: 18,
-                      ),
-                      suffixIcon: Icon(
-                        Icons.mail_outline,
-                        size: 30,
-                      )),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.01,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.tealAccent),
-                          borderRadius: BorderRadius.circular(5)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      hintText: '********',
-                      hintStyle: TextStyle(
-                        fontSize: 18,
-                      ),
-                      suffixIcon: Icon(
-                        FontAwesomeIcons.lockOpen,
-                      )),
-                )
-              ],
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.01,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Row(
+      body: SingleChildScrollView(
+              child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
+              Text(
+                "Sign In",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
+              Column(
                 children: [
-                  Checkbox(
-                    value: _checkedValue,
-                    onChanged: (newValue) {
-                      setState(() {
-                        _checkedValue = newValue;
-                      });
-                    },
-                    // controlAffinity:
-                    //     ListTileControlAffinity.leading, //  <-- leading Checkbox
+                  TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.tealAccent),
+                            borderRadius: BorderRadius.circular(5)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        hintText: 'sarahkaramsi@gmail.com',
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.mail_outline,
+                          size: 30,
+                        )),
                   ),
                   SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.01,
+                    height: MediaQuery.of(context).size.height * 0.01,
                   ),
-                  Text(
-                    'Stay Signed In',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.tealAccent),
+                            borderRadius: BorderRadius.circular(5)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        hintText: '********',
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                        ),
+                        suffixIcon: Icon(
+                          FontAwesomeIcons.lockOpen,
+                        )),
+                  )
                 ],
               ),
-              InkWell(
-                child: Text(
-                  'Forget Password ?',
-                  style: TextStyle(
-                      fontSize: 16, decoration: TextDecoration.underline),
-                ),
-              )
-            ]),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            ButtonTheme(
-              minWidth: MediaQuery.of(context).size.width * 0.4,
-              height: MediaQuery.of(context).size.height * 0.05,
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => BottomAppbarScreen()));
-                },
-                color: Colors.tealAccent,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                child: Text(
-                  "SIGN IN",
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
-                ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.01,
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            Text(
-              "or sign in with",
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.02,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RawMaterialButton(
-                  onPressed: () {},
-                  elevation: 2.0,
-                  child: SvgPicture.asset(
-                    'assets/icon/facebook.svg',
-                    color: Colors.grey,
-                  ),
-                  padding: EdgeInsets.all(15.0),
-                  shape: CircleBorder(side: BorderSide(color: Colors.blue)),
-                ),
-                RawMaterialButton(
-                  onPressed: () {},
-                  elevation: 5.0,
-                  child: SvgPicture.asset(
-                    'assets/icon/google_plus.svg',
-                    color: Colors.grey,
-                  ),
-                  padding: EdgeInsets.all(15.0),
-                  shape: CircleBorder(side: BorderSide(color: Colors.red)),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account?",
-                  style: TextStyle(fontSize: 16),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Row(
+                  children: [
+                    Checkbox(
+                      value: _checkedValue,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _checkedValue = newValue;
+                        });
+                      },
+                      // controlAffinity:
+                      //     ListTileControlAffinity.leading, //  <-- leading Checkbox
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.01,
+                    ),
+                    Text(
+                      'Stay Signed In',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
                 ),
                 InkWell(
-                  onTap: () {
+                  child: Text(
+                    'Forget Password ?',
+                    style: TextStyle(
+                        fontSize: 16, decoration: TextDecoration.underline),
+                  ),
+                )
+              ]),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              ButtonTheme(
+                minWidth: MediaQuery.of(context).size.width * 0.4,
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: RaisedButton(
+                  onPressed: () {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => RegisterScreen()));
+                            builder: (context) => BottomAppbarScreen()));
                   },
+                  color: Colors.tealAccent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
                   child: Text(
-                    "Register",
+                    "SIGN IN",
                     style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.tealAccent,
-                        decoration: TextDecoration.underline),
+                        fontSize: 17,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
                   ),
-                )
-              ],
-            )
-          ],
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Text(
+                "or sign in with",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RawMaterialButton(
+                    onPressed: () {},
+                    elevation: 2.0,
+                    child: SvgPicture.asset(
+                      'assets/icon/facebook.svg',
+                      color: Colors.grey,
+                    ),
+                    padding: EdgeInsets.all(15.0),
+                    shape: CircleBorder(side: BorderSide(color: Colors.blue)),
+                  ),
+                  RawMaterialButton(
+                    onPressed: () {},
+                    elevation: 5.0,
+                    child: SvgPicture.asset(
+                      'assets/icon/google_plus.svg',
+                      color: Colors.grey,
+                    ),
+                    padding: EdgeInsets.all(15.0),
+                    shape: CircleBorder(side: BorderSide(color: Colors.red)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterScreen()));
+                    },
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.tealAccent,
+                          decoration: TextDecoration.underline),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
-      ),
+      )),
     );
   }
 }
--- lib/register.dart
+++ lib/register.dart
@@ -10,102 +10,127 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Register"),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.tealAccent),
-                          borderRadius: BorderRadius.circular(5)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      hintText: 'First Name',
-                      suffixIcon: Icon(Icons.person_outline, size: 30)),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.01,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.tealAccent),
-                          borderRadius: BorderRadius.circular(5)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      hintText: 'Last Name',
-                      suffixIcon: Icon(Icons.person_outline, size: 30)),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.01,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.tealAccent),
-                          borderRadius: BorderRadius.circular(5)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      hintText: 'Email',
-                      suffixIcon: Icon(
-                        Icons.mail_outline,
-                        size: 30,
-                      )),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.01,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.tealAccent),
-                          borderRadius: BorderRadius.circular(5)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      hintText: 'Password',
-                      suffixIcon: Icon(FontAwesomeIcons.lockOpen)),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.01,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.tealAccent),
-                          borderRadius: BorderRadius.circular(5)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      hintText: 'Confirm Password',
-                      suffixIcon: Icon(FontAwesomeIcons.lockOpen)),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            ButtonTheme(
-              minWidth: MediaQuery.of(context).size.width * 0.4,
-              height: MediaQuery.of(context).size.height * 0.05,
-              child: RaisedButton(
-                onPressed: () {},
-                color: Colors.tealAccent,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                child: Text(
-                  "REGISTER",
-                  style: TextStyle(color: Colors.black),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
+              Text(
+                "Register",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.tealAccent),
+                            borderRadius: BorderRadius.circular(5)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        hintText: 'First Name',
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                        ),
+                        suffixIcon: Icon(Icons.person_outline, size: 30)),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.tealAccent),
+                            borderRadius: BorderRadius.circular(5)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        hintText: 'Last Name',
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                        ),
+                        suffixIcon: Icon(Icons.person_outline, size: 30)),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.tealAccent),
+                            borderRadius: BorderRadius.circular(5)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        hintText: 'Email',
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.mail_outline,
+                          size: 30,
+                        )),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.tealAccent),
+                            borderRadius: BorderRadius.circular(5)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        hintText: 'Password',
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                        ),
+                        suffixIcon: Icon(FontAwesomeIcons.lockOpen)),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.tealAccent),
+                            borderRadius: BorderRadius.circular(5)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        hintText: 'Confirm Password',
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                        ),
+                        suffixIcon: Icon(FontAwesomeIcons.lockOpen)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              ButtonTheme(
+                minWidth: MediaQuery.of(context).size.width * 0.4,
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.tealAccent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Text(
+                    "REGISTER",
+                    style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
