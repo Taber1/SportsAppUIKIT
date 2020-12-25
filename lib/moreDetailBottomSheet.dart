80894c23e2a8bec0d8951d755c87243009896e20
Taber Bin Zameer <tbzameer@gmail.com>
2020-12-25 23:55:55 +0500

    more detail page bottom sheet

--- lib/moreDetailBottomSheet.dart
+++ lib/moreDetailBottomSheet.dart
@@ -2,181 +2,265 @@ import 'package:flutter/material.dart';
 
 class ModalTrigger extends StatelessWidget {
   moreDeatilBottomSheet(context) {
-    showModalBottomSheet(
+    showModalBottomSheet<dynamic>(
+        isScrollControlled: true,
         context: context,
         builder: (BuildContext context) {
-          return Container(
-            height: MediaQuery.of(context).size.height * 0.9,
-            decoration: BoxDecoration(
-                color: Colors.grey[900],
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(70),
-                    topRight: Radius.circular(70))),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.035,
-                  ),
-                  Text(
-                    "More Details",
-                    style: TextStyle(
-                        fontSize: 22,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.035,
-                  ),
-                  Container(
-                      width: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.only(left: 10),
-                      child: Text(
-                        "Opening Hours",
-                        style: TextStyle(color: Colors.grey, fontSize: 18),
-                      )),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.035,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.tealAccent),
-                          borderRadius: BorderRadius.circular(5)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      hintText: '10 Am -10 Pm',
-                    ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.035,
-                  ),
-                  Container(
-                      width: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.only(left: 10),
-                      child: Text(
-                        "Available",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold),
-                      )),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.035,
-                  ),
-                  Row(
+          return Wrap(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.8,
+                decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(70),
+                        topRight: Radius.circular(70))),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
                     children: [
                       SizedBox(
-                        width: 30,
+                        height: MediaQuery.of(context).size.height * 0.035,
                       ),
-                      ButtonTheme(
-                        minWidth: MediaQuery.of(context).size.width * 0.25,
-                        height: MediaQuery.of(context).size.height * 0.05,
-                        child: RaisedButton(
-                          onPressed: () {},
-                          color: Colors.grey[800],
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Text(
-                            "Parking",
-                            style: TextStyle(color: Colors.grey, fontSize: 18),
-                          ),
+                      Center(
+                        child: Text(
+                          "More Details",
+                          style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
                         ),
                       ),
                       SizedBox(
-                        width: 30,
+                        height: MediaQuery.of(context).size.height * 0.035,
                       ),
-                      ButtonTheme(
-                        minWidth: MediaQuery.of(context).size.width * 0.15,
-                        height: MediaQuery.of(context).size.height * 0.05,
-                        child: RaisedButton(
-                          onPressed: () {},
-                          color: Colors.grey[800],
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
+                      Container(
+                          width: MediaQuery.of(context).size.width,
+                          padding: EdgeInsets.only(left: 10),
                           child: Text(
-                            "Wifi",
+                            "Opening Hours",
                             style: TextStyle(color: Colors.grey, fontSize: 18),
-                          ),
+                          )),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.035,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.tealAccent),
+                              borderRadius: BorderRadius.circular(5)),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5)),
+                          hintText: '10 Am -10 Pm',
                         ),
                       ),
                       SizedBox(
-                        width: 30,
+                        height: MediaQuery.of(context).size.height * 0.035,
                       ),
-                      ButtonTheme(
-                        minWidth: MediaQuery.of(context).size.width * 0.25,
-                        height: MediaQuery.of(context).size.height * 0.05,
-                        child: RaisedButton(
-                          onPressed: () {},
-                          color: Colors.grey[800],
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
+                      Container(
+                          width: MediaQuery.of(context).size.width,
+                          padding: EdgeInsets.only(left: 10),
                           child: Text(
-                            "Security",
-                            style: TextStyle(color: Colors.grey, fontSize: 18),
-                          ),
-                        ),
+                            "Available",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold),
+                          )),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.035,
                       ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 30,
+                          ),
+                          ButtonTheme(
+                            minWidth: MediaQuery.of(context).size.width * 0.25,
+                            height: MediaQuery.of(context).size.height * 0.05,
+                            child: RaisedButton(
+                              onPressed: () {},
+                              color: Colors.grey[800],
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Text(
+                                "Parking",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 18),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          ButtonTheme(
+                            minWidth: MediaQuery.of(context).size.width * 0.15,
+                            height: MediaQuery.of(context).size.height * 0.05,
+                            child: RaisedButton(
+                              onPressed: () {},
+                              color: Colors.grey[800],
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Text(
+                                "Wifi",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 18),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          ButtonTheme(
+                            minWidth: MediaQuery.of(context).size.width * 0.25,
+                            height: MediaQuery.of(context).size.height * 0.05,
+                            child: RaisedButton(
+                              onPressed: () {},
+                              color: Colors.grey[800],
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Text(
+                                "Security",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.015,
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 30,
+                          ),
+                          ButtonTheme(
+                            minWidth: MediaQuery.of(context).size.width * 0.45,
+                            height: MediaQuery.of(context).size.height * 0.05,
+                            child: RaisedButton(
+                              onPressed: () {},
+                              color: Colors.grey[800],
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Text(
+                                "Changing Room",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 18),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          ButtonTheme(
+                            minWidth: MediaQuery.of(context).size.width * 0.3,
+                            height: MediaQuery.of(context).size.height * 0.05,
+                            child: RaisedButton(
+                              onPressed: () {},
+                              color: Colors.grey[800],
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Text(
+                                "Restaurant",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.05,
+                      ),
+                      Container(
+                          width: MediaQuery.of(context).size.width,
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            "Rules",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold),
+                          )),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.05,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '1.',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                              child: Text(
+                            'Dummy text is also used to demostrate the appearance of different typefaces.',
+                            style: TextStyle(color: Colors.grey),
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.justify,
+                            maxLines: 2,
+                            softWrap: true,
+                          ))
+                        ],
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.01,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '2.',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                              child: Text(
+                            'Dummy text is also used to demostrate the appearance of different typefaces.',
+                            style: TextStyle(color: Colors.grey),
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.justify,
+                            maxLines: 2,
+                            softWrap: true,
+                          ))
+                        ],
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.01,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '3.',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                              child: Text(
+                            'Dummy text is also used to demostrate the appearance of different typefaces.',
+                            style: TextStyle(color: Colors.grey),
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.justify,
+                            maxLines: 2,
+                            softWrap: true,
+                          ))
+                        ],
+                      )
                     ],
                   ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.015,
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 30,
-                      ),
-                      ButtonTheme(
-                        minWidth: MediaQuery.of(context).size.width * 0.45,
-                        height: MediaQuery.of(context).size.height * 0.05,
-                        child: RaisedButton(
-                          onPressed: () {},
-                          color: Colors.grey[800],
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Text(
-                            "Changing Room",
-                            style: TextStyle(color: Colors.grey, fontSize: 18),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      ButtonTheme(
-                        minWidth: MediaQuery.of(context).size.width * 0.3,
-                        height: MediaQuery.of(context).size.height * 0.05,
-                        child: RaisedButton(
-                          onPressed: () {},
-                          color: Colors.grey[800],
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Text(
-                            "Restaurant",
-                            style: TextStyle(color: Colors.grey, fontSize: 18),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.05,
-                  ),
-                  Container(
-                      width: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.only(left: 10),
-                      child: Text(
-                        "Rules",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold),
-                      )),
-                ],
-              ),
-            ),
+                ),
+              )
+            ],
           );
         });
   }
