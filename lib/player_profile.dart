1f79cfdd75280116ad68208cd8f4d30a9330d840
Taber Bin Zameer <tbzameer@gmail.com>
2020-12-25 08:59:17 +0500

    person profile complete

--- lib/player_profile.dart
+++ lib/player_profile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+enum q1Ans { Yes, No }
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -12,6 +15,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   var ageselected;
   List<DropdownMenuItem<String>> gender = [];
   var genderselected;
+  q1Ans _ans1 = q1Ans.Yes;
 
   void ansWer() {
     ans = [];
@@ -233,8 +237,243 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 height: MediaQuery.of(context).size.height * 0.02,
               ),
               Row(
-                children: [EachSport()],
-              )
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  EachSport(
+                    icon: Icons.sports_cricket,
+                    color: Colors.redAccent,
+                  ),
+                  EachSport(
+                    icon: Icons.sports_football,
+                    color: Colors.tealAccent,
+                  ),
+                  EachSport(
+                    icon: Icons.sports_tennis,
+                    color: Colors.grey[900],
+                  ),
+                  EachSport(
+                    icon: Icons.sports_hockey,
+                    color: Colors.grey[900],
+                  ),
+                  EachSport(
+                    icon: FontAwesomeIcons.swimmer,
+                    color: Colors.yellowAccent,
+                  )
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.01,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  EachSport(
+                    icon: Icons.sports_rugby,
+                    color: Colors.grey[900],
+                  ),
+                  EachSport(
+                    icon: Icons.sports_soccer,
+                    color: Colors.grey[900],
+                  ),
+                  EachSport(
+                    icon: Icons.sports_volleyball,
+                    color: Colors.blueAccent,
+                  ),
+                  EachSport(
+                    icon: Icons.sports_basketball,
+                    color: Colors.grey[900],
+                  ),
+                  EachSport(
+                    icon: FontAwesomeIcons.dumbbell,
+                    color: Colors.grey[900],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Divider(
+                indent: 10,
+                thickness: 1.0,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    "Are you willing to buddy up?",
+                    style: TextStyle(fontSize: 22),
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: q1Ans.Yes,
+                    groupValue: _ans1,
+                    onChanged: (value) {
+                      setState(() {
+                        _ans1 = value;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Yes',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  Radio(
+                      value: q1Ans.No,
+                      groupValue: _ans1,
+                      onChanged: (value) {
+                        setState(() {
+                          _ans1 = value;
+                        });
+                      }),
+                  Text(
+                    'No',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Divider(
+                indent: 10,
+                thickness: 1.0,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    "Are you a coach?",
+                    style: TextStyle(fontSize: 22),
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: q1Ans.Yes,
+                    groupValue: _ans1,
+                    onChanged: (value) {
+                      setState(() {
+                        _ans1 = value;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Yes',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  Radio(
+                      value: q1Ans.No,
+                      groupValue: _ans1,
+                      onChanged: (value) {
+                        setState(() {
+                          _ans1 = value;
+                        });
+                      }),
+                  Text(
+                    'No',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    "Please share a link",
+                    style: TextStyle(),
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.tealAccent),
+                      borderRadius: BorderRadius.circular(5)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.only(left: 10),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Upload file",
+                        style: TextStyle(fontSize: 22),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.2,
+                      ),
+                      ButtonTheme(
+                        minWidth: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.05,
+                        child: RaisedButton(
+                          onPressed: () {},
+                          color: Colors.tealAccent,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Text(
+                            "UPLOAD",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Divider(
+                indent: 10,
+                thickness: 1.0,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              ButtonTheme(
+                minWidth: MediaQuery.of(context).size.width * 0.4,
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.tealAccent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Text(
+                    "NEXT",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.04,
+              ),
             ],
           ),
         ),
@@ -245,7 +484,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
 class EachSport extends StatefulWidget {
   final IconData icon;
-  EachSport({this.icon});
+  final Color color;
+  EachSport({this.icon, this.color});
   @override
   _EachSportState createState() => _EachSportState();
 }
@@ -255,12 +495,12 @@ class _EachSportState extends State<EachSport> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.grey[900], borderRadius: BorderRadius.circular(30)),
-      height: MediaQuery.of(context).size.height * 0.1,
-      width: MediaQuery.of(context).size.width * 0.2,
+          color: widget.color, borderRadius: BorderRadius.circular(30)),
+      height: MediaQuery.of(context).size.height * 0.085,
+      width: MediaQuery.of(context).size.width * 0.18,
       child: Center(
         child: Icon(
-          Icons.sports_cricket,
+          widget.icon,
           size: 30,
         ),
       ),
